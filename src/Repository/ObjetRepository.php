8e196401bc3234635df09cb9970d2dab0549732f
Wafimo3108 <cassimwael31@gmail.com>
2025-04-15 22:50:41 +0200

    Update ObjetRepository.php

--- src/Repository/ObjetRepository.php
+++ src/Repository/ObjetRepository.php
@@ -5,14 +5,6 @@ use App\Entity\Objet;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<Objet>
- *
- * @method Objet|null find($id, $lockMode = null, $lockVersion = null)
- * @method Objet|null findOneBy(array $criteria, array $orderBy = null)
- * @method Objet[]    findAll()
- * @method Objet[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
 class ObjetRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
