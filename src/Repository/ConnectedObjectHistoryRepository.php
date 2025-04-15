3c72bd9588d12f933ce14971d4ab2486184f834e
Wafimo3108 <cassimwael31@gmail.com>
2025-04-15 22:41:55 +0200

    Update ConnectedObjectHistoryRepository.php

--- src/Repository/ConnectedObjectHistoryRepository.php
+++ src/Repository/ConnectedObjectHistoryRepository.php
@@ -6,9 +6,6 @@ use App\Entity\ConnectedObjectHistory;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
-/**
- * @extends ServiceEntityRepository<ConnectedObjectHistory>
- */
 class ConnectedObjectHistoryRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
